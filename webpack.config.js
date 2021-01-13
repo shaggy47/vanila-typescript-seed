5a4e36c60e3e0a81132067f1119391d68dcfd35e
shaggy47 <vaibhavpfatkal@gmail.com>
2021-01-13 10:21:16 +0530

    removed extra import

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 'use strict';
 const path = require('path');
 const htmlWebPackPlugin = require('html-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
     devtool: 'inline-source-map',
