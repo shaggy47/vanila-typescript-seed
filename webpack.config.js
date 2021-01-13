de7cb73fed6441b44fc2cf9a2558874f050cd57d
shaggy47 <vaibhavpfatkal@gmail.com>
2021-01-13 10:16:10 +0530

    Fixed webpack errors

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+'use strict';
+const path = require('path');
+const htmlWebPackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'inline-source-map',
+	entry: './src/app/index.ts',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: '/node_modules/'
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin()],
+    resolve: {
+        extensions: [ '.ts', '.tsx', '.js' ]
+    }
+};
